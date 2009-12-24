7248261a887c69f9943dffb16b64a8ee6403d89f
Martin Rubli <martin_rubli@logitech.com>
2009-12-24 08:39:32 +0000

    - uvcdynctrl: Update udev scripts to work with newer (but not older) versions of udev. udev >= 141 should work now. - makedoc.sh: Remove an obsolete automake hack.
    git-svn-id: svn://localhost/qct/Linux@30 aba6e6d2-d720-d247-b230-8903cd0051a9

--- makedoc.sh
+++ makedoc.sh
@@ -3,6 +3,5 @@ rm -f doxygen/default.cfg &&
 ln -s libwebcam.cfg doxygen/default.cfg &&
 rm -rf doxygen-output &&
 mkdir -p doxygen-output/libwebcam &&
-sed -i -r -e 's/^\s*(rm -rf doxygen-output)$/# Automake sucks ... $1/' Makefile &&
 make doxygen-doc &&
 cp -v doxygen/res/* doxygen-output/libwebcam/html/
