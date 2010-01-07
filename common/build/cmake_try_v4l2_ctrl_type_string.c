1fa8a2a7449f18720ab7a05a4b0f85186c03b581
Martin Rubli <martin_rubli@logitech.com>
2010-01-07 02:11:28 +0000

    More thorough checking for the presence of V4L2 string controls
    git-svn-id: svn://localhost/qct/webcam-tools/trunk@51 aba6e6d2-d720-d247-b230-8903cd0051a9

--- common/build/cmake_try_v4l2_ctrl_type_string.c
+++ common/build/cmake_try_v4l2_ctrl_type_string.c
@@ -1,3 +1,9 @@
 #include <linux/videodev2.h>
 
-int main() { V4L2_CTRL_TYPE_STRING; }
+int main()
+{
+	v4l2_ctrl_type t = V4L2_CTRL_TYPE_STRING;
+	struct v4l2_ext_control s;
+	s.size;
+	s.string;
+}
